21b150648d08173c666edcc309a6f4d2ded0cbdc
Emiliano Guerrero <164534739+Emiliano-Guerrero-Blanco-1067@users.noreply.github.com>
2025-02-07 16:56:10 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,17 @@
+//funcion que no devuelve valor
+//funcion saludar 
+void saludar(){
+    print ("Hola mundo");
+}
+
+void escribenombre(){
+  print("Jorge Emiliano Guerrero Blanco");
+}
+
+void main(){
+  print("Jorge Emiliano Guerrero Blanco Mat: 22308051281067");
+  print("Llamando a la funcion saludar");
+    saludar();
+    print("Llamando a la funcion escribenombre");
+    escribenombre();
+}
