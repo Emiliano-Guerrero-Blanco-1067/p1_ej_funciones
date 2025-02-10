809fcaf7b663130302c580a2786e3de33c6aaaa7
Emiliano Guerrero <164534739+Emiliano-Guerrero-Blanco-1067@users.noreply.github.com>
2025-02-10 18:46:38 +0000

    funcion param

---
+++ funcionparam.dart
@@ -0,0 +1,16 @@
+int sumar (int a, int b) {
+  return a + b;
+}
+String concatenar (String a, String b) {
+  return a + b;
+}
+
+void main () {
+  print("Emiliano Guerrero Blanco 22308051281067");
+  print("Llamando a la función sumar");
+  print("La suma es: ${sumar(10, 5)}");
+  print("Llamando a la función concatenar");
+  print("La concatenación es: ${concatenar("Hola ", "Mundo")}");
+}
+
+
--- recuros/funcionsuma.md
+++ recuros/funcionsuma.md
@@ -3,3 +3,6 @@ EJemplo de salida
 
 ejemplo de salida funcion normal + funcion flecha
 ![alt text](image-1.png)
+
+ejemplo de salida de dos ejemplos de funciones con parametros posicionales
+![alt text](image-2.png)
